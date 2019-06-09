780e09335cf181533e5b9046286b6d5a284d7717
Leon520 <1506816370@qq.com>
2019-06-09 23:09:01 +0800

    1、修改def

--- Server/Server1/Def.h
+++ Server/Server1/Def.h
@@ -4,6 +4,17 @@
 
 using namespace std;
 
+// 日志类型
+enum ELogType
+{
+	Info,	// 信息
+	Debug,	// 调试
+	Error,	// 错误
+	Fatal,	// 严重错误
+
+	None	// 无
+};
+
 // 客户端连接信息
 typedef struct tagClientConnInfo
 {
@@ -11,3 +22,4 @@ typedef struct tagClientConnInfo
 	int port;			// 端口
 	int socket;			// 
 }ClientConnInfo;
+
